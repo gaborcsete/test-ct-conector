10e11b1527b966456b634a4d1c993275c0307adc
gcsete <gabor.csete@ayatacommerce.com>
2023-10-12 13:39:15 +0200

    Return a action

--- service/src/routes/service.route.ts
+++ service/src/routes/service.route.ts
@@ -8,8 +8,13 @@ serviceRouter.post('/', async (req, res) => {
   logger.info(JSON.stringify(req.body));
   logger.info(req.body.action + ' ' + req.body.resource.typeId);
 
+  const response =
+    '{  "version": ' +
+    req.body.resource.obj.version +
+    ' ,  "actions": [  {   "action" : "setCustomField",  "name" : "notes",  "value" : "Wrap flag is set to true so wrapping required" } ] }';
+
   res.status(200);
-  res.send();
+  res.send(response);
 });
 
 export default serviceRouter;
